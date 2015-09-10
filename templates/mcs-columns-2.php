9a18879bfcc66190f9cf25dbdf87a4caca4897a7
Maxwell Morgan <mmorgan@linchpinagency.com>
2015-09-10 09:18:18 -0400

    Frontend markup for column offset

--- templates/mcs-columns-2.php
+++ templates/mcs-columns-2.php
@@ -10,15 +10,25 @@
  * @subpackage Templates
  */
 ?>
+<?php $offset = (int) get_post_meta( get_the_ID(), '_mcs_offset', true ); ?>
 
 <section <?php post_class() ?> <?php mcs_section_background(); ?>>
 	<div class="row">
 		<?php $blocks = mcs_get_section_blocks( get_the_ID() ); ?>
-		<?php foreach ( $blocks as $block ) : ?>
-			<div class="small-12 medium-<?php esc_attr_e( get_post_meta( $block->ID, '_mcs_column_width', true ) ); ?> columns">
+		<?php $i = 1; foreach ( $blocks as $block ) : ?>
+
+			<?php
+				$column_width = get_post_meta( $block->ID, '_mcs_column_width', true );
+
+				if ( 2 == $i && $offset ) :
+			?>
+			<div class="small-12 medium-<?php esc_attr_e( $column_width - $offset ); ?> medium-offset-<?php esc_attr_e( $offset ); ?> columns">
+			<?php else: ?>
+			<div class="small-12 medium-<?php esc_attr_e( $column_width ); ?> columns">
+			<?php endif; ?>
 				<h2 class="entry-title"><?php the_title(); ?></h2>
 				<?php echo apply_filters( 'the_content', $block->post_content ); ?>
 			</div>
-		<?php endforeach; ?>
+		<?php $i++; endforeach; ?>
 	</div>
 </section>
