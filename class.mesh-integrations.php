a722700787c1619eb95f9ec41d208875af79f0d3
Aaron Ware <aware@linchpinagency.com>
2017-07-27 11:41:03 -0400

    Added error logging when an integration can not be found. This will help with debugging if a user has created their own integration.

--- class.mesh-integrations.php
+++ class.mesh-integrations.php
@@ -32,6 +32,8 @@ class Mesh_Integrations {
 			foreach ( $this->integrations as $file ) {
 				if ( file_exists( $file ) ) {
 					include_once $file;
+				} else {
+					error_log( sprintf( '%s %s', __( 'Mesh - Unable to locate integration: ', 'mesh' ), $file ) );
 				}
 			}
 		}
